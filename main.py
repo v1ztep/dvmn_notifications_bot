efa44a639b85995da1b74c49cfc0b6c136c3e7e9
Vladimir <v1ztep@gmail.com>
2021-04-28 22:01:08 +0300

    Обработал ошибки

--- main.py
+++ main.py
@@ -10,28 +10,35 @@ def get_response(url, params=None, headers=None):
     return response
 
 
-def main():
-    load_dotenv()
-    dvmn_token = os.getenv('DEVMAN_TOKEN')
-
+def check_verified_work(dvmn_token):
     dvmn_api_url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': f'Token {dvmn_token}',
     }
     params = {
-        'timestamp': None
+        'timestamp': None,
     }
 
     while True:
-        response = get_response(dvmn_api_url, headers=headers, params=params)
-        response_detail = response.json()
-        print(response_detail)
-        status = response_detail['status']
-        if status == 'found':
-            params['timestamp'] = response_detail['last_attempt_timestamp']
-        if status == 'timeout':
-            params['timestamp'] = response_detail['timestamp_to_request']
+        try:
+            response = get_response(dvmn_api_url, headers=headers, params=params)
+            response_detail = response.json()
+            print(response_detail)
+            status = response_detail['status']
+            if status == 'found':
+                params['timestamp'] = response_detail['last_attempt_timestamp']
+            elif status == 'timeout':
+                params['timestamp'] = response_detail['timestamp_to_request']
+        except requests.exceptions.ReadTimeout:
+            print('timeout')
+            continue
 
+
+def main():
+    load_dotenv()
+    dvmn_token = os.getenv('DEVMAN_TOKEN')
+
+    check_verified_work(dvmn_token)
 
 
 if __name__ == '__main__':
